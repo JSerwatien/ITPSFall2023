d5305ad57b265e26c26e65cc9456f9662ae79827
Joshua Serwatien <josh@greatideafactory.com>
2023-12-26 08:24:26 -0600

    Added Save button on priority page

--- ITPSFall2023.Entity/TicketSurveyEntity.cs
+++ ITPSFall2023.Entity/TicketSurveyEntity.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ITPSFall2023.Entity
+﻿namespace ITPSFall2023.Entity
 {
     public class TicketSurveyEntity
     {
